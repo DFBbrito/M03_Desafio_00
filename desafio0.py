097072d05a6d8865ebc0ecefdb0e859b1b8cb676
DFBbrito <a24567@esenviseu.net>
2024-01-03 12:10:01 +0000

    exercicio2

--- desafio0.py
+++ desafio0.py
@@ -3,17 +3,26 @@
 Por exemplo: 
     Hello - deve mostrar l
 """
+
 def LetrasIguais(palavra):
     for posicao in range(len(palavra)-1):
         if palavra[posicao]==palavra[posicao+1]:
             print(palavra[posicao])
 
 LetrasIguais('Hello')
-
+print('-----')
 """
 2 - Cria uma função que recebe três números. Esta função deve mostrar, para os números passados, quantos são pares.
 """
+def Pares(n1,n2,n3):
+    if n1%2==0:
+        print('par')
+    if n2%2==0:
+        print('par')
+    if n3%2==0:
+        print('par')
 
+Pares(6,20,8)
 
 
 """
